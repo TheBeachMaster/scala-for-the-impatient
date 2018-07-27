a9aa1dee146ad1405722ef4f75d262f30305d83a
TheBeachmaster <otienoken@outlook.com>
2018-07-27 23:40:33 +0300

    Add Basic control structure logic

--- chapter2/control_structures_and_functions/src/main/scala/Main.scala
+++ chapter2/control_structures_and_functions/src/main/scala/Main.scala
@@ -36,8 +36,53 @@ object Main extends App {
     // Get user input and print it out
     val vcs = StdIn.readLine("Enter your fav VCS ")
     // Other methods from the StdIn class do not take prompts
+    // Other memebrs of the StdIn class readByte, readShort, readBoolean, readChar
     print("Number of repos: ")
     val numRepo = StdIn.readInt()
     println(s"Looks like you like ${vcs} with ${numRepo} repos")
+
+    // Loops in scala are not commonly used
+    // Scala does not have for(init; test;update) construct
+    val n : Int = 10
+    var r : Int = 1
+    for(i <- 1 to n)
+    {
+        r = r * i
+        println(s"Current value of i is $i")
+    } 
+
+
+    // Let's traverse a string
+    val s : String = "Hello huge string"
+    var sum : Int = 10
+    for(i <- 0 to s.length -1)
+    {
+        sum += s(i)
+        println(s"Sum is $s and index is ${s(i)}")
+    }
+
+    // Using generators in the form var<-expr
+    for(i <- 1 to 3; j <- 1 to 3)
+    {
+        println(f"${10 * i +j}%3d")
+    }
+
+    // You can setup giards (booleans) for gens as so
+    for(i <- 1 to 3; j <- 1 to 3 if i != j)
+    {
+        println(f"${10 * i +j}%3d")
+    }
+
+    // Introduce definitions
+    for(i <- 1 to 3; ran = 4 - i; j <- ran to 3)  
+    {
+        println(f"${10 * i + j}%3d")
+    }
+
+    // Comprehension loops
+    for(i <- 1 to 10) yield i % 3
+
+    for(c <- 0 to 1; i <- "Whatsup") yield (i + c).toChar
+
 
 }
